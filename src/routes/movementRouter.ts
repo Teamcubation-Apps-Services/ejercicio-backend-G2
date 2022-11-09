a5f8625ec06bd399c4a9b80c1150a80713b554c9
julia zack <juliazack@MBP-de-julia.fibertel.com.ar>
2022-11-09 12:00:18 -0300

    queries movement router

--- src/routes/movementRouter.ts
+++ src/routes/movementRouter.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express'
-import { getAllMovementsController } from '../controllers/movementController'
+import { getAllMovementsController, createMovementController, updateMovementController, deleteMovementController } from '../controllers/movementController'
 
 const movements = Router()
 
 movements.route('/').get(getAllMovementsController)
+movements.route('/').post(createMovementController)
+movements.route('/:id').put(updateMovementController)
+movements.route('/:id').delete(deleteMovementController)
+
 
 export default movements
