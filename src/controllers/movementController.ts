e59c1f454eb47f25a09b43ff411d1c5d7a71dfb9
julia zack <juliazack@MBP-de-julia.fibertel.com.ar>
2022-11-09 11:58:27 -0300

    queries movement controller

--- src/controllers/movementController.ts
+++ src/controllers/movementController.ts
@@ -1,12 +1,37 @@
 import { NextFunction, Request, Response } from 'express'
-import { getAllMovements } from '../services/movementService'
+import { getAllMovements, createMovement, updateMovement, deleteMovement } from '../services/movementService'
 
 export const getAllMovementsController = async (_: Request, res: Response, next: NextFunction): Promise<void> => {
   try {
     await getAllMovements(_, res)
     next()
   } catch (e) {
-    // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+    console.log(`Error: ${e}`)
+  }
+}
+export const createMovementController = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  try {
+    await createMovement(req, res)
+    next()
+  } catch (e) {
+    console.log(`Error: ${e}`)
+  }
+}
+
+export const updateMovementController = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  try {
+    await updateMovement(req, res)
+    next()
+  } catch (e) {
+    console.log(`Error: ${e}`)
+  }
+}
+
+export const deleteMovementController = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  try {
+    await deleteMovement(req, res)
+    next()
+  } catch (e) {
     console.log(`Error: ${e}`)
   }
 }
