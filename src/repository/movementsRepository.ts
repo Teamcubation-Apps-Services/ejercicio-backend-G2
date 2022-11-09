07c5b30de31cc319da1702894414b2c7ded21593
julia zack <juliazack@MBP-de-julia.fibertel.com.ar>
2022-11-09 11:57:20 -0300

    queries movement repository & service

--- src/repository/movementsRepository.ts
+++ src/repository/movementsRepository.ts
@@ -6,3 +6,35 @@ const prisma = new SqlClient()
 export const getAllMovementsRepository = async (req: Request, res: Response): Promise<Movement[]> => {
   return await prisma.movement.findMany({})
 }
+export const createMovementRepository = async (req: Request, res: Response): Promise<Movement> => {
+  const { id, type } = req.body
+  return await prisma.movement.create({
+    data: {
+      id,
+      type
+    }
+  })
+}
+
+export const updateMovementRepository = async (req: Request, res: Response): Promise<Movement> => {
+  const data = req.body
+  const { id } = req.params
+  return await prisma.movement.update({
+    where: {
+      id: Number(id)
+    },
+    data
+  })
+}
+
+export const deleteMovementRepository = async (req: Request, res: Response): Promise<Movement> => {
+  const { id } = req.params
+  return await prisma.movement.update({
+    where: {
+      id: Number(id)
+    },
+    data: {
+      isActive: false
+    }
+  })
+}
--- src/services/movementService.ts
+++ src/services/movementService.ts
@@ -1,7 +1,22 @@
 import { Request, Response } from 'express'
-import { getAllMovementsRepository } from '../repository/movementsRepository'
+import { createMovementRepository, deleteMovementRepository, getAllMovementsRepository, updateMovementRepository } from '../repository/movementsRepository'
 
 export const getAllMovements = async (req: Request, res: Response): Promise<void> => {
   const movements = await getAllMovementsRepository(req, res)
   res.status(200).json(movements)
+}
+
+export const createMovement = async (req: Request, res: Response): Promise<void> => {
+  const movement = await createMovementRepository(req, res)
+  res.status(201).json(movement)
+}
+
+export const updateMovement = async (req: Request, res: Response): Promise<void> => {
+  await updateMovementRepository(req, res)
+  res.status(204)
+}
+
+export const deleteMovement = async (req: Request, res: Response): Promise<void> => {
+  await deleteMovementRepository(req, res)
+  res.status(204)
 }
