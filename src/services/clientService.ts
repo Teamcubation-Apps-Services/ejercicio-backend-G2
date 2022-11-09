dab5263e19fcdd8b05f0b58863bb1b52736ad9bf
julia zack <juliazack@MBP-de-julia.fibertel.com.ar>
2022-11-09 10:13:45 -0300

    queries client service

--- src/services/clientService.ts
+++ src/services/clientService.ts
@@ -1,7 +1,22 @@
 import { Request, Response } from 'express'
-import { getAllClientRepository } from '../repository/clientsRepository'
+import { getAllClientRepository, createClientRepository, updateClientRepository, deleteClientRepository } from '../repository/clientsRepository'
 
 export const getAllClients = async (req: Request, res: Response): Promise<void> => {
   const clients = await getAllClientRepository(req, res)
   res.status(200).json(clients)
+}
+
+export const createClient = async (req: Request, res: Response): Promise<void> => {
+  const client = await createClientRepository(req, res)
+  res.status(201).json(client)
+}
+
+export const updateClient = async (req: Request, res: Response): Promise<void> => {
+  await updateClientRepository(req, res)
+  res.status(204)
+}
+
+export const deleteClient = async (req: Request, res: Response): Promise<void> => {
+  await deleteClientRepository(req, res)
+  res.status(204)
 }
