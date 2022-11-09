6df4e655bddf000186c828e72364ed499f034293
julia zack <juliazack@MBP-de-julia.fibertel.com.ar>
2022-11-09 10:10:09 -0300

    queries client repository

--- src/repository/clientsRepository.ts
+++ src/repository/clientsRepository.ts
@@ -6,3 +6,40 @@ const prisma = new SqlClient()
 export const getAllClientRepository = async (req: Request, res: Response): Promise<Client[]> => {
   return await prisma.client.findMany({})
 }
+
+export const createClientRepository = async (req: Request, res: Response): Promise<Client> => {
+  const { id, dni, firstName, lastName, email, phoneNumber } = req.body
+  return await prisma.client.create({
+    data: {
+      id,
+      dni,
+      firstName,
+      lastName,
+      email,
+      phoneNumber
+    }
+  })
+}
+
+export const updateClientRepository = async (req: Request, res: Response): Promise<Client> => {
+  const data = req.body
+  const { id } = req.params
+  return await prisma.client.update({
+    where: {
+      id: Number(id)
+    },
+    data
+  })
+}
+
+export const deleteClientRepository = async (req: Request, res: Response): Promise<Client> => {
+  const { id } = req.params
+  return await prisma.client.update({
+    where: {
+      id: Number(id)
+    },
+    data: {
+      isActive: false
+    }
+  })
+}
