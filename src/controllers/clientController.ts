35bf97e29d2a335746a541639f1b32e7cadcad0e
julia zack <juliazack@MBP-de-julia.fibertel.com.ar>
2022-11-09 10:15:54 -0300

    queries client controller

--- src/controllers/clientController.ts
+++ src/controllers/clientController.ts
@@ -1,12 +1,38 @@
 import { NextFunction, Request, Response } from 'express'
-import { getAllClients } from '../services/clientService'
+import { getAllClients, createClient, updateClient, deleteClient } from '../services/clientService'
 
 export const getAllClientsController = async (_: Request, res: Response, next: NextFunction): Promise<void> => {
   try {
     await getAllClients(_, res)
     next()
   } catch (e) {
-    // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+    console.log(`Error: ${e}`)
+  }
+}
+
+export const createClientController = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  try {
+    await createClient(req, res)
+    next()
+  } catch (e) {
+    console.log(`Error: ${e}`)
+  }
+}
+
+export const updateClientController = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  try {
+    await updateClient(req, res)
+    next()
+  } catch (e) {
+    console.log(`Error: ${e}`)
+  }
+}
+
+export const deleteClientController = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  try {
+    await deleteClient(req, res)
+    next()
+  } catch (e) {
     console.log(`Error: ${e}`)
   }
 }
