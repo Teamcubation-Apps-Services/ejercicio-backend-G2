f9d70a81743b7a94febba0110f693d8eca8932d1
julia zack <juliazack@MBP-de-julia.fibertel.com.ar>
2022-11-16 09:49:12 -0300

    client repository catching errors added

--- src/repository/clientsRepository.ts
+++ src/repository/clientsRepository.ts
@@ -3,43 +3,85 @@ import { PrismaClient as SqlClient, Client } from '../../prisma/generated/sql-cl
 
 const prisma = new SqlClient()
 
-export const getAllClientRepository = async (req: Request, res: Response): Promise<Client[]> => {
-  return await prisma.client.findMany({})
+export const getAllClientRepository = async (req: Request, res: Response): Promise<Client[] | Error> => {
+  try {
+    return await prisma.client.findMany({
+      where: {
+        isActive: true
+      }
+    })
+  } catch (e: any) {
+    return new Error(e.meta.cause)
+  }
 }
 
-export const createClientRepository = async (req: Request, res: Response): Promise<Client> => {
+export const createClientRepository = async (req: Request, res: Response): Promise<Client | Error> => {
   const { id, dni, firstName, lastName, email, phoneNumber } = req.body
-  return await prisma.client.create({
-    data: {
-      id,
-      dni,
-      firstName,
-      lastName,
-      email,
-      phoneNumber
-    }
-  })
+  try {
+    return await prisma.client.create({
+      data: {
+        id,
+        dni,
+        firstName,
+        lastName,
+        email,
+        phoneNumber
+      }
+    })
+  } catch (e: any) {
+    return new Error(e.meta.cause)
+  }
 }
 
-export const updateClientRepository = async (req: Request, res: Response): Promise<Client> => {
-  const data = req.body
-  const { id } = req.params
-  return await prisma.client.update({
-    where: {
-      id: Number(id)
-    },
-    data
-  })
+export const updateClientRepository = async (req: Request, res: Response): Promise<Client | Error> => {
+  try {
+    const data = req.body
+    const { id } = req.params
+    const client = await prisma.client.findUnique({
+      where: {
+        id: Number(id)
+      }
+    })
+    if (client !== null && client.isActive) {
+      return await prisma.client.update({
+        where: {
+          id: Number(id)
+        },
+        data
+      })
+    } else {
+      return new Error('Invalid id')
+    }
+  } catch (e: any) {
+    return new Error(e.meta.cause)
+  }
 }
 
-export const deleteClientRepository = async (req: Request, res: Response): Promise<Client> => {
-  const { id } = req.params
-  return await prisma.client.update({
-    where: {
-      id: Number(id)
-    },
-    data: {
-      isActive: false
+export const deleteClientRepository = async (req: Request, res: Response): Promise<Client | Error> => {
+  try {
+    const { id } = req.params
+    const client = await prisma.client.findUnique({
+      where: {
+        id: Number(id)
+      },
+    })
+    if (client !== null) {
+      if (client.isActive) {
+        return await prisma.client.update({
+          where: {
+            id: Number(id)
+          },
+          data: {
+            isActive: false
+          }
+        })
+      } else {
+        return new Error('Client already deleted')
+      }
+    } else {
+      return new Error('Record not found')
     }
-  })
+  } catch (e: any) {
+    return new Error(e.meta.cause)
+  }
 }
