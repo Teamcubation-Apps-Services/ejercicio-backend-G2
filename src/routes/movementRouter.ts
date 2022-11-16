ccfc87dba4793f496aa42c1e816f536fea8b71ea
julia zack <juliazack@MBP-de-julia.fibertel.com.ar>
2022-11-16 11:16:47 -0300

    swagger added to movementRouter

--- src/routes/movementRouter.ts
+++ src/routes/movementRouter.ts
@@ -3,6 +3,109 @@ import { getAllMovementsController, createMovementController, updateMovementCont
 
 const movements = Router()
 
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *    MovementBody:
+ *      type: object
+ *      properties:
+ *        type:
+ *          type: string
+ *    MovementResponse:
+ *      type: object
+ *      properties:
+ *        id:
+ *          type: integer
+ *        type:
+ *          type: string
+ *        isActive:
+ *          type: boolean
+ */
+/**
+  * @swagger
+  * tags:
+  *   name: movements
+ */
+/**
+ * @swagger
+ * /movements:
+ *  get:
+ *    summary: List all movements
+ *    tags: [movements]
+ *    responses:
+ *      200:
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                $ref: '#/components/schemas/MovementResponse'
+ *  post:
+ *    summary: Create a new movement
+ *    tags: [movements]
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/MovementBody'
+ *    responses:
+ *      201:
+ *        description: Created movement
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/MovementResponse'
+ */
+/**
+ * @swagger
+ * /movements/{id}:
+ *  put:
+ *    summary: Update a movement
+ *    tags: [movements]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: integer
+ *        required: true
+ *        description: Id of the movement to update
+ *    requestBody:
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/MovementBody'
+ *    responses:
+ *      200:
+ *        description: Updated movement
+ *        content:
+ *          application/json:
+ *            schema:
+ *                $ref: '#/components/schemas/MovementResponse'
+ *  delete:
+ *    summary: Delete a movement
+ *    tags: [movements]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: integer
+ *        required: true
+ *        description: Id of the movement to delete
+ *    responses:
+ *      200:
+ *        description: Deleted movement
+ *        content:
+ *          application/json:
+ *            schema:
+ *                $ref: '#/components/schemas/MovementResponse'
+ *            example:
+ *                id: 4
+ *                type: transfer
+ *                isActive: false
+ */
+
 movements.route('/').get(getAllMovementsController)
 movements.route('/').post(createMovementController)
 movements.route('/:id').put(updateMovementController)
