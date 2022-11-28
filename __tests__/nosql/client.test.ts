bf764de94b2cc9fce2038c54997f1138d2dbf837
julia zack <juliazack@MBP-de-julia.fibertel.com.ar>
2022-11-28 11:24:59 -0300

    nosql client test created

--- __tests__/nosql/client.test.ts
+++ __tests__/nosql/client.test.ts
@@ -2,14 +2,14 @@ import app from '../../src/app'
 import supertest from 'supertest'
 
 it('Should return an array with at least 1 record, and status code 200', async () => {
-  const response = await supertest(app).get('/clients/1')
+  const response = await supertest(app).get('/nosql/clients/1')
 
   expect(response.body.length).toBeGreaterThan(0)
   expect(response.statusCode).toBe(200)
 })
 
 it('Should create a client', async () => {
-  const response = await supertest(app).post('/clients').send({ dni: 31164331, firstName: 'Matias', lastName: 'Kosoy', email: 'holaprobando@gmail.com', phoneNumber: 12341234 })
+  const response = await supertest(app).post('/nosql/clients').send({ dni: 31164331, firstName: 'Matias', lastName: 'Kosoy', email: 'holaprobando@gmail.com', phoneNumber: 12341234 })
   expect(response.statusCode).toBe(201)
   expect(response.body.dni).toBe(31164331)
   expect(response.body.firstName).toBe('Matias')
@@ -20,13 +20,13 @@ it('Should create a client', async () => {
 })
 
 it('Should update a record', async () => {
-  const response = await supertest(app).put('/clients/1').send({ email: 'holacambiemimail@gmail.com' })
+  const response = await supertest(app).put('/nosql/clients/1').send({ email: 'holacambiemimail@gmail.com' })
   expect(response.statusCode).toBe(200)
   expect(response.body.email).toBe('holacambiemimail@gmail.com')
 })
 
 it.skip('Should delete a record', async () => {
-  const response = await supertest(app).delete('/clients/1')
+  const response = await supertest(app).delete('/nosql/clients/1')
 
   expect(response.statusCode).toBe(200)
   expect(response.body.isActive).toBe(false)
