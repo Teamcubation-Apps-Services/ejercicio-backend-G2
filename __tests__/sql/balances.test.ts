3957e8620e2e2966a8ac9401af4b2f109c750808
lserra-iov <lucio.serra@iovlabs.org>
2022-11-29 13:43:21 +0100

    Fix balances and movements tests

--- __tests__/sql/balances.test.ts
+++ __tests__/sql/balances.test.ts
@@ -2,14 +2,14 @@ import app from '../../src/app'
 import supertest from 'supertest'
 
 it('Should return an array with at least 1 record, and status code 200', async() => {
-    const response = await supertest(app).get('/balances/1')
+    const response = await supertest(app).get('/sql/balances/1')
     
     expect(response.body.length).toBeGreaterThan(0)
     expect(response.statusCode).toBe(200)
 })
 
 it('Should create a balance', async() => {
-    const response = await supertest(app).post('/balances').send({clientId: 1, coinId: 3, balance: 100})
+    const response = await supertest(app).post('/sql/balances').send({clientId: 1, coinId: 3, balance: 100})
     expect(response.statusCode).toBe(201)
     expect(response.body.clientId).toBe(1)
     expect(response.body.coinId).toBe(3)
@@ -18,19 +18,19 @@ it('Should create a balance', async() => {
 })
 
 it('Should not create a negative balance', async() => {
-    const response = await supertest(app).post('/balances/1/2').send({balance: -20})
+    const response = await supertest(app).post('/sql/balances/1/2').send({balance: -20})
 
     expect(response.statusCode).toBe(404)
 })
 
 it('Should update a record', async () => {
-    const response = await supertest(app).put('/balances/1/2').send({balance: 222})
+    const response = await supertest(app).put('/sql/balances/1/2').send({balance: 222})
     expect(response.statusCode).toBe(200)
     expect(response.body.balance).toBe("222")
 })
 
-it.skip('Should delete a record', async() => {
-    const response = await supertest(app).delete('/balances/1/2')
+it('Should delete a record', async() => {
+    const response = await supertest(app).delete('/sql/balances/1/3')
     
     expect(response.statusCode).toBe(200)
     expect(response.body.isActive).toBe(false)
--- __tests__/sql/movement.test.ts
+++ __tests__/sql/movement.test.ts
@@ -1,15 +1,18 @@
 import app from '../../src/app'
 import supertest from 'supertest'
 
+let idResponse: string;
+
 it('Should return an array with at least 1 record, and status code 200', async () => {
-  const response = await supertest(app).get('/movements/1')
+  const response = await supertest(app).get('/sql/movements/1')
 
   expect(response.body.length).toBeGreaterThan(0)
   expect(response.statusCode).toBe(200)
 })
 
 it('Should create a movement', async() => {
-  const response = await supertest(app).post('/movements').send({ type: 'purchase' })
+  const response = await supertest(app).post('/sql/movements').send({ type: 'purchase' })
+  idResponse = response.body.id
   expect(response.statusCode).toBe(201)
   expect(response.body.type).toBe('purchase')
   expect(response.body.isActive).toBe(true)
@@ -17,13 +20,13 @@ it('Should create a movement', async() => {
 
 
 it('Should update a record', async () => {
-  const response = await supertest(app).put('/movements/1').send({ type: 'investment' })
+  const response = await supertest(app).put(`/sql/movements/${idResponse}`).send({ type: 'investment' })
   expect(response.statusCode).toBe(200)
-  expect(response.body.balance).toBe('investment')
+  expect(response.body.type).toBe('investment')
 })
 
-it.skip('Should delete a record', async() => {
-  const response = await supertest(app).delete('/movements/1')
+it('Should delete a record', async() => {
+  const response = await supertest(app).delete(`/sql/movements/${idResponse}`)
     
   expect(response.statusCode).toBe(200)
   expect(response.body.isActive).toBe(false)
