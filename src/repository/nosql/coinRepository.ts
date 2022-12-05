156f0f6dc55090b4193a9b2d85110be2e6e083a1
julia zack <juliazack@MBP-de-julia.fibertel.com.ar>
2022-12-05 12:27:21 -0300

    refactor coin nosql repository

--- src/repository/nosql/coinRepository.ts
+++ src/repository/nosql/coinRepository.ts
@@ -4,7 +4,7 @@ import { PrismaClient as NoSqlClient, Coin } from '../../../prisma/generated/nos
 const prisma = new NoSqlClient()
 
 export const getAllCoinRepository = async (req: Request, res: Response): Promise<Coin[] | Error> => {
-  try{
+  try {
     return await prisma.coin.findMany({
       where: {
         isActive: true
@@ -15,20 +15,20 @@ export const getAllCoinRepository = async (req: Request, res: Response): Promise
   }
 }
 
-export const getCoinRepository = async (req: Request, res: Response) : Promise<Coin[] | Error> => {
-  try{
+export const getCoinRepository = async (req: Request, res: Response): Promise<Coin[] | Error> => {
+  try {
     const id = req.params.id
-  
-    return await prisma.coin.findMany({where: {id}})
-  }catch(e: any){
+
+    return await prisma.coin.findMany({ where: { id } })
+  } catch (e: any) {
     return new Error(e.meta?.cause)
   }
 }
 
-export const postCoinRepository = async (req: Request, res: Response) : Promise<Coin | Error> => {
-  try{
+export const postCoinRepository = async (req: Request, res: Response): Promise<Coin | Error> => {
+  try {
     const { name, description, quotationReference, annualPerformance } = req.body
-    
+
     return await prisma.coin.create({
       data: {
         name,
@@ -37,57 +37,56 @@ export const postCoinRepository = async (req: Request, res: Response) : Promise<
         annualPerformance
       }
     })
-  }catch(e: any){
+  } catch (e: any) {
     return new Error(e.meta?.cause)
   }
-  
 }
 
-export const updateCoinRepository = async (req: Request, res: Response) : Promise<Coin | Error> => {
-  try{
+export const updateCoinRepository = async (req: Request, res: Response): Promise<Coin | Error> => {
+  try {
     const data = req.body
     const id = req.params.id
-    const coin = await prisma.coin.findUnique({where: {id}})
-    if (coin !== null && coin.isActive) {
-      return await prisma.coin.update({
-        where: {
-          id: id
-        },
-        data
-      })
-    } else {
+    const coin = await prisma.coin.findUnique({ where: { id } })
+    if (coin === null) {
+      return new Error('Record not found')
+    }
+    if (!coin.isActive) {
       return new Error('Invalid id')
     }
-  }catch(e: any){
+    return await prisma.coin.update({
+      where: {
+        id
+      },
+      data
+    })
+  } catch (e: any) {
     return new Error(e.meta?.cause)
   }
 }
- 
-export const deleteCoinRepository = async (req: Request, res: Response) : Promise<Coin | Error> => {
-  try{
+
+export const deleteCoinRepository = async (req: Request, res: Response): Promise<Coin | Error> => {
+  try {
     const { id } = req.params
     const coin = await prisma.coin.findUnique({
       where: {
         id
-      },
-    })
-    if (coin) {
-      if (coin.isActive) {
-        return await prisma.coin.update({
-          where: {
-            id
-          },
-          data: {
-            isActive: false
-          }
-        })
-      } else {
-        return new Error('Coin already deleted')
       }
-    } else {
+    })
+    if (coin === null) {
       return new Error('Record not found')
     }
-  }catch(e: any){
+    if (!coin.isActive) {
+      return new Error('Coin already deleted')
+    }
+    return await prisma.coin.update({
+      where: {
+        id
+      },
+      data: {
+        isActive: false
+      }
+    })
+  } catch (e: any) {
     return new Error(e.meta?.cause)
   }
 }
