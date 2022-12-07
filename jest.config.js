7630c2c61bee51a07afa6be959bd65b397a8e338
lserra-iov <lucio.serra@iovlabs.org>
2022-12-07 14:53:18 +0100

    Add test coverage command

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,9 @@
 module.exports= {
     preset: 'ts-jest',
     testEnvironment: 'node',
+    collectCoverageFrom: [
+        '<rootDir>/src/routes/**/*.ts',
+        '<rootDir>/src/controllers/**/*.ts',
+        '<rootDir>/src/services/**/*.ts',
+      ],
 };
